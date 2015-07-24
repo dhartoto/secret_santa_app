a4c6153840122a0544b9b0f0c44cddcada8edf9f
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 15:49:09 +1000

    Add getter for status in DataImporter
    Add getter and setter method for status.
    'sets status to 200' test passing.
    - 4 tests failing.
    - 5 tests pending.

--- app/services/data_importer.rb
+++ app/services/data_importer.rb
@@ -1,7 +1,9 @@
 class DataImporter
-  attr_accessor :error_message
-  
+  attr_accessor :status, :error_message
+
   def self.import(file_name, year)
+    uploader = FileUploader.upload(file_name)
+    new()
   end
 
   def successful?
