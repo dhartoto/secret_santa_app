b87c4b8b985c052ea2c971685dbf497c2a136f39
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 19:22:19 +1000

    Update FileUploader
    Implement file upload when file is valid.
    2 tests now passing.

--- app/services/file_uploader.rb
+++ app/services/file_uploader.rb
@@ -2,7 +2,15 @@ class FileUploader
   attr_accessor :status, :error_message
 
   def self.upload(file)
-    new()
+    File.open(Rails.root.join('public', 'uploads',
+      file.original_filename), 'wb') do |content|
+        content.write(file.read)
+      end
+    new(status: 200)
+  end
+
+  def initialize(options={})
+    @status = options[:status]
   end
 
   def successful?
