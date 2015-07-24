d523615f3a89b3d1b80898006637a6387e7ec0ba
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 15:12:05 +1000

    Add tests for DataImporter
    Add failing tests:
    - 'validates file' test
    - 'create instance of data importer' test
    
    Add 9 pending tests for valid and invalid files

--- spec/services/data_importer_spec.rb
+++ spec/services/data_importer_spec.rb
@@ -9,6 +9,28 @@ describe DataImporter do
     end
 
     it { should respond_to :error_message}
+
+    it 'validates file' do
+      expect(Validator).to receive(:validate)
+      importer = DataImporter.import('some_file.csv', 2015)
+    end
+    it 'creates an instance of data importer' do
+      importer = DataImporter.import('some_file.csv', 2015)
+      expect(importer).to be_an_instance_of(DataImporter)
+    end
+    context 'when valid' do
+      it 'saves year'
+      it 'saves participants'
+      it 'creates secret santas'
+      it 'sets status to 200'
+    end
+    context 'when invalid' do
+      it 'does not save year'
+      it 'does not save participants'
+      it 'does not create secret santas'
+      it 'assigns error_message'
+      it 'sets status to 400'
+    end
   end
 
   describe '#successful?' do
