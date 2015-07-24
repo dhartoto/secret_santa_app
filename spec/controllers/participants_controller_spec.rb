3d49bce037484b240ebdb6f4786d6a7d138af202
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 14:06:21 +1000

    Add failing tests to participants#create
    Add failing tests for:
    - importing participants csv file.
    - invalid file
    - valid file

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -14,5 +14,25 @@ describe ParticipantsController do
       post :create, year: 2015
       expect(response).to redirect_to :root
     end
+    it 'imports data' do
+      expect(DataImporter).to receive(:import)
+      post :create, year: 2015
+    end
+    context 'when file is valid' do
+      it 'display success message' do
+        importer = OpenStruct.new(valid?: true)
+        allow(DataImporter).to receive(:import).and_return(importer)
+        post :create, year: 2015
+        expect(flash[:success]).to eq("File uploaded successfully")
+      end
+    end
+    context 'when file is not valid' do
+      it 'display failure message' do
+        importer = OpenStruct.new(valid?:false, error_message: "Computer says no!")
+        allow(DataImporter).to receive(:import).and_return(importer)
+        post :create, year: 2015
+        expect(flash[:success]).to eq("File uploaded successfully")
+      end
+    end
   end
 end
