fd79b5486199ab1743abcc68bed53d39c409292f
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 13:42:04 +1000

    Add failing test for participants#create

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -8,4 +8,11 @@ describe ParticipantsController do
       expect(response.status).to eq(200)
     end
   end
+
+  describe '#create' do
+    it 'request successful' do
+      post :create, year: 2015
+      expect(response.status).to eq(200)
+    end
+  end
 end
