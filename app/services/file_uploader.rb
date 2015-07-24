f98231b8887b9e754b7dd4a574730e36f10d262b
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 20:05:48 +1000

    Update FileUploader
    Add guard for no file uploaded.
    2 tests passing.

--- app/services/file_uploader.rb
+++ app/services/file_uploader.rb
@@ -3,7 +3,7 @@ class FileUploader
 
   def self.upload(file, year)
     year = Year.find_by(year: year) if file
-    if year.present? || file.content_type != 'text/csv'
+    if year.present? || survey_file.blank? || file.content_type != 'text/csv'
       error_message = error_message(file)
       new(status: 400, error_message: error_message)
     else
@@ -26,7 +26,9 @@ class FileUploader
   private
 
   def self.error_message(file)
-    if file.content_type != 'text/csv'
+    if file.blank?
+      'No file selected. Please upload a CSV file.'
+    elsif file.content_type != 'text/csv'
       'Invalid file. Please upload a CSV file.'
     else
       'Participants list for this year already exists.'
