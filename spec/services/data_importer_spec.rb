d24f8bf1668a40e6bfa0a5d7c963adca072d8856
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 15:23:59 +1000

    Update DataImporter spec
    Update spec to reflect change of implementation. Add FileUploader
    to process and move Validator out of DataImporter.
    
    Tests still failing with 9 pending.

--- spec/services/data_importer_spec.rb
+++ spec/services/data_importer_spec.rb
@@ -7,24 +7,24 @@ describe DataImporter do
       expect(DataImporter).to respond_to(:import)
       importer = DataImporter.import('some_file.csv', 2015)
     end
-
     it { should respond_to :error_message}
 
-    it 'validates file' do
-      expect(Validator).to receive(:validate)
-      importer = DataImporter.import('some_file.csv', 2015)
-    end
     it 'creates an instance of data importer' do
       importer = DataImporter.import('some_file.csv', 2015)
       expect(importer).to be_an_instance_of(DataImporter)
     end
-    context 'when valid' do
+    it 'updloads file' do
+      expect(FileUploader).to receive(:upload)
+      DataImporter.import('some_file.csv', 2015)
+    end
+
+    context 'when file upload successful' do
+      it 'sets status to 200'
       it 'saves year'
       it 'saves participants'
       it 'creates secret santas'
-      it 'sets status to 200'
     end
-    context 'when invalid' do
+    context 'when file upload unsuccesful' do
       it 'does not save year'
       it 'does not save participants'
       it 'does not create secret santas'
