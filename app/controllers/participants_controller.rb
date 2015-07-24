93d7708aad7de851e76e5e273ce5949072c4482e
Ardito Hartoto <dhartoto@gmail.com>
2015-07-24 14:45:04 +1000

    Update participants#create
    Complete implementation of create action.
    Add DataImporter to create action.
    Add success and failure message to display to user.
    Mock DataImporter and stub methods (.import, #successful?,
    error_message)
    All tests in participants#create passing.

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -3,6 +3,19 @@ class ParticipantsController < ApplicationController
   def index; end
 
   def create
+    importer = DataImporter.import(params[:file_name], params[:year])
+    importer.successful? ? display_success_message : display_failure_message(importer)
     redirect_to :root
   end
+
+  private
+
+  def display_success_message
+    flash[:success] = "File uploaded successfully"
+  end
+
+  def display_failure_message(importer)
+    flash[:alert] = importer.error_message
+  end
+
 end
--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -11,27 +11,32 @@ describe ParticipantsController do
 
   describe '#create' do
     it 'redirects to root' do
-      post :create, year: 2015
+      importer = instance_double('DataImporter', successful?: true)
+      allow(DataImporter).to receive(:import).and_return(importer)
+      post :create, file_name: 'file.csv', year: 2015
       expect(response).to redirect_to :root
     end
     it 'imports data' do
+      importer = instance_double('DataImporter', successful?: true)
+      allow(DataImporter).to receive(:import).and_return(importer)
       expect(DataImporter).to receive(:import)
       post :create, year: 2015
     end
     context 'when file is valid' do
       it 'display success message' do
-        importer = OpenStruct.new(valid?: true)
+        importer = instance_double('DataImporter', successful?: true)
         allow(DataImporter).to receive(:import).and_return(importer)
-        post :create, year: 2015
+        post :create, file_name: 'file.csv', year: 2015
         expect(flash[:success]).to eq("File uploaded successfully")
       end
     end
     context 'when file is not valid' do
       it 'display failure message' do
-        importer = OpenStruct.new(valid?:false, error_message: "Computer says no!")
+        importer = instance_double('DataImporter',
+          successful?: false, error_message: "Computer says no!")
         allow(DataImporter).to receive(:import).and_return(importer)
-        post :create, year: 2015
-        expect(flash[:success]).to eq("File uploaded successfully")
+        post :create, file_name: 'file.csv', year: 2015
+        expect(flash[:alert]).to eq("Computer says no!")
       end
     end
   end
