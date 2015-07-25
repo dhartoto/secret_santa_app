d9fe63e1161865c773c48e8456a437ab43faad18
Ardito Hartoto <dhartoto@gmail.com>
2015-07-25 10:39:15 +1000

    Add failing tests for SecretSantaCreator
    Add initial failing test to build out allocation of Secret
    Santas.

--- spec/services/secret_santa_creator_spec.rb
+++ spec/services/secret_santa_creator_spec.rb
@@ -3,5 +3,24 @@ require 'rails_helper'
 describe SecretSantaCreator do
   describe '.create' do
     it { should be_an_instance_of(SecretSantaCreator) }
+
+    let(:year) { Fabricate(:year, year: 2015) }
+    let(:file) { mock_file(file_name: 'participants.csv', type: 'text/csv') }
+
+    before { FileUploader.upload(file , 2015) }
+    after { File.delete('public/uploads/participants.csv') }
+
+    it 'should create a secret santa for each person' do
+      SecretSantaCreator.create(year)
+      expect(SecretSanta.count).to eq(5)
+    end
+    it 'should not be themselves'
+    it 'should not be their partners'
+    it 'should not not have duplicate secret santas'
+    it 'should not be the same as last year'
+
+    context 'when no Secret Santa last year'
+
   end
+
 end
