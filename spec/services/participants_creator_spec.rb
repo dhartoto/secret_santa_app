944d518b253b469a191edef172bd5db5e48c631c
Ardito Hartoto <dhartoto@gmail.com>
2015-07-25 18:00:21 +1000

    Add failing spec to ParticipantsCreator spec
    Current implementation duplicates participants when a new year
    is added. Add failing tests to capture this.

--- spec/services/participants_creator_spec.rb
+++ spec/services/participants_creator_spec.rb
@@ -11,16 +11,32 @@ describe ParticipantsCreator do
     it { should be_an_instance_of(ParticipantsCreator) }
   end
 
-  it 'saves participants from file' do
-    ParticipantsCreator.create('participants.csv', year)
-    expect(Participant.count).to eq(5)
+  context 'when participants do not exist' do
+    it 'saves participants from file' do
+      ParticipantsCreator.create('participants.csv', year)
+      expect(Participant.count).to eq(5)
+    end
+    it 'saves participants full name' do
+      ParticipantsCreator.create('participants.csv', year)
+      expect(Participant.first.full_name).to eq('Homer Simpson')
+    end
+    it 'saves participants partners full name' do
+      ParticipantsCreator.create('participants.csv', year)
+      expect(Participant.first.partner_full_name).to eq('Marge Simpson')
+    end
   end
-  it 'saves participants full name' do
-    ParticipantsCreator.create('participants.csv', year)
-    expect(Participant.first.full_name).to eq('Homer Simpson')
-  end
-  it 'saves participants partners full name' do
-    ParticipantsCreator.create('participants.csv', year)
-    expect(Participant.first.partner_full_name).to eq('Marge Simpson')
+
+  context 'when particpants exist' do
+    it 'does not create duplicates' do
+      Fabricate(:participant, full_name: 'Homer Simpson')
+      ParticipantsCreator.create('participants.csv', year)
+      expect(Participant.count).to eq(5)
+    end
+    it 'updates year for all participants' do
+      Fabricate(:participant, full_name: 'Homer Simpson')
+      ParticipantsCreator.create('participants.csv', year)
+      participants = Participant.where(year: year)
+      expect(participants.count).to eq(5)
+    end
   end
 end
