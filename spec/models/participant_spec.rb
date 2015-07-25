98b26215f5dddf9a7420046def8dcce14f527968
Ardito Hartoto <dhartoto@gmail.com>
2015-07-25 14:09:46 +1000

    Add has_many association to secret_santa

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Participant do
   it { should belong_to :year }
+  it { should have_many :secret_santa }
 end
--- spec/models/year_spec.rb
+++ spec/models/year_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Year do
   it { should have_many :participants }
+  it { should have_many :secret_santa }
 end
