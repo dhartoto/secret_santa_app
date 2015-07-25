818d636b777bd1362fc3a5f30519f5dcae5352fd
Ardito Hartoto <dhartoto@gmail.com>
2015-07-25 19:11:07 +1000

    Add SecretSantaResultsController
    Added show action and show template to pass intial test.
    Initial test passing now.

---
+++ app/controllers/secret_santa_results_controller.rb
@@ -0,0 +1,5 @@
+class SecretSantaResultsController < ApplicationController
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'participants#index'
   resources :participants, only: :create
+  resources :secret_santa_results, only: :show
 end
