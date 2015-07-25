80438b3a0c2ed94521782d69b4d08f92a08fc161
Ardito Hartoto <dhartoto@gmail.com>
2015-07-26 09:32:39 +1000

    Add failing tests for results#show
    Added failing tests for available results and unavailable results
    paths.

--- spec/controllers/results_controller_spec.rb
+++ spec/controllers/results_controller_spec.rb
@@ -6,6 +6,22 @@ describe ResultsController do
       get :show, id: 1
       expect(response.status).to eq(200)
     end
+    context 'when results are available' do
+      it 'returns result for the year requested' do
+        year = Fabricate(:year)
+        get :show, id: year.id
+        expect(assigns(:year).count).to eq(1)
+      end
+    end
+    context 'when results are not available' do
+      it 'displays error message' do
+        get :show, id: 1
+        expect(assigns(:year).count).to eq(1)
+      end
+      it 'redirects back to home page' do
+        get :show, id: 1
+        expect(response).to redirect_to :root
+      end
+    end
   end
-
 end
