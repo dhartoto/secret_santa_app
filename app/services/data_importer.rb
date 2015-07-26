f0a6ab0f17fb0f7dcb12ca9266be7c47b4979474
Ardito Hartoto <dhartoto@gmail.com>
2015-07-26 18:39:04 +1000

    Refactor DataImporter
    Move ParticipantsCreator and SecretSantaCreator to global var
    and loop through CREATORS as both respond to the same message.

--- app/services/data_importer.rb
+++ app/services/data_importer.rb
@@ -1,14 +1,15 @@
 class DataImporter
+  CREATORS = [ParticipantsCreator, SecretSantaCreator]
+
   attr_accessor :status, :error_message
 
   def self.import(file, year)
     uploader = FileUploader.upload(file, 2015)
     if uploader.successful?
       year = Year.create(year: year)
-      participants = ParticipantsCreator.new(year)
-      secret_santa = SecretSantaCreator.new(year)
-      participants.create
-      secret_santa.create
+      CREATORS.each do |creator|
+        creator.new(year).create
+      end
       new(status: 200)
     else
       new(status: 400, error_message: uploader.error_message)
