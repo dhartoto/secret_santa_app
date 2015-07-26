8f358aa047ab96d001c8dd052e125f3161a719bb
Ardito Hartoto <dhartoto@gmail.com>
2015-07-26 10:31:41 +1000

    Fix bug in SecretSanta #last_year
    Previous implementation selects the correct year but not scoped to
    the participant.
    Added participant fabricator to test and changed method to search
    through participant's association.

--- app/models/secret_santa.rb
+++ app/models/secret_santa.rb
@@ -3,6 +3,6 @@ class SecretSanta < ActiveRecord::Base
   belongs_to :participant
 
   def last_year
-    SecretSanta.find_by(year_id: (year_id - 1))
+    participant.secret_santa.find_by(year_id: (year_id - 1))
   end
 end
